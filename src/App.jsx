62a2a7827851d9997e56817cc7033952bd031a8f
Muhammadazeem37 <140012372+Muhammadazeem37@users.noreply.github.com>
2025-08-04 11:21:34 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { Route, Routes } from 'react-router-dom'
 import Websplash from './Components/WebComponents/Websplash/Websplash'
+import Login from './Auth/WebAuth/Login/Login'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Websplash />} />
+        <Route path="/Login" element={<Login />} />
       </Routes>
     </>
   )
