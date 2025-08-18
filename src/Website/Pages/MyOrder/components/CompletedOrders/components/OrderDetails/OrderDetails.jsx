e6119b008204005d856bc17e66cfdeb4c9be1d42
Noor Usman <noorusman0097@gmail.com>
2025-08-18 17:47:53 +0500

    orderupdate

--- src/Website/Pages/MyOrder/components/CompletedOrders/components/OrderDetails/OrderDetails.jsx
+++ src/Website/Pages/MyOrder/components/CompletedOrders/components/OrderDetails/OrderDetails.jsx
@@ -19,7 +19,164 @@ function OrderDetails() {
         </p>
       </div>
 
-      <div className="bg-white  py-3 px-3 rounded-lg">
+     <div className="mt-5 space-y-4">
+         <div className="bg-white py-3 px-3 rounded-lg">
+        {/* Product Info */}
+        <div className="flex flex-col xl:flex-row items-center justify-between gap-3 lg:gap-6">
+          <img
+            src={bpimg6}
+            alt="Advanced Dissection Kit"
+            className="h-40 w-40 sm:h-48 sm:w-48 lg:h-50 lg:w-50 rounded-xl mx-auto xl:mx-0 flex-shrink-0"
+          />
+
+          <div className="w-full flex flex-col xl:flex-row items-start xl:justify-center gap-6 ">
+            <div className="flex-1 ">
+              <div className="py-3 flex flex-col lg:flex-row gap-4">
+                <p className="text-[#333333] font-medium text-lg sm:text-xl lg:text-2xl leading-tight pr-6 text-center xl:text-left">
+                  Automatic Blood Pressure
+                </p>
+              </div>
+
+              {/* Details */}
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 lg:grid-cols-7 gap-4 lg:gap-6 ">
+                <div>
+                  <p className="text-[#8f8f8f] text-sm">Brand:</p>
+                  <p className="font-semibold text-[#333333] text-[24px]">
+                    Rossmax
+                  </p>
+                </div>
+                <div>
+                  <p className="text-[#8f8f8f] text-sm">Country of Origin:</p>
+                  <p className="font-semibold text-[#333333] text-[24px]">
+                    America
+                  </p>
+                </div>
+                <div>
+                  <p className="text-[#8f8f8f] text-sm">Total Price:</p>
+                  <p className="font-[500] text-[24px] cursor-pointer text-nowrap">
+                    $100.00
+                  </p>
+                </div>
+                <div>
+                  <p className="text-[#8f8f8f] text-sm">Quantity:</p>
+                  <p className="font-[500] text-[24px] cursor-pointer text-nowrap">
+                    6
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="px-10 flex items-center gap-2">
+            <p className="font-semibold text-[#333333] text-4xl">120</p>
+            <img src={pointimg} alt="" className="w-8 " />
+          </div>
+        </div>
+      </div>
+         <div className="bg-white  py-3 px-3 rounded-lg">
+        {/* Product Info */}
+        <div className="flex flex-col xl:flex-row items-center justify-between gap-3 lg:gap-6">
+          <img
+            src={bpimg6}
+            alt="Advanced Dissection Kit"
+            className="h-40 w-40 sm:h-48 sm:w-48 lg:h-50 lg:w-50 rounded-xl mx-auto xl:mx-0 flex-shrink-0"
+          />
+
+          <div className="w-full flex flex-col xl:flex-row items-center xl:justify-center gap-6 ">
+            <div className="flex-1 ">
+              <div className="py-3 flex flex-col lg:flex-row gap-4">
+                <p className="text-[#333333] font-medium text-lg sm:text-xl lg:text-2xl leading-tight pr-6 text-center xl:text-left">
+                  Automatic Blood Pressure
+                </p>
+              </div>
+
+              {/* Details */}
+              <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-7 gap-4 lg:gap-6 ">
+                <div>
+                  <p className="text-[#8f8f8f] text-sm">Brand:</p>
+                  <p className="font-semibold text-[#333333] text-[24px]">
+                    Rossmax
+                  </p>
+                </div>
+                <div>
+                  <p className="text-[#8f8f8f] text-sm">Country of Origin:</p>
+                  <p className="font-semibold text-[#333333] text-[24px]">
+                    America
+                  </p>
+                </div>
+                <div>
+                  <p className="text-[#8f8f8f] text-sm">Total Price:</p>
+                  <p className="font-[500] text-[24px] cursor-pointer text-nowrap">
+                    $100.00
+                  </p>
+                </div>
+               <div>
+                  <p className="text-[#8f8f8f] text-sm">Quantity:</p>
+                  <p className="font-[500] text-[24px] cursor-pointer text-nowrap">
+                    6
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="px-10 flex items-center gap-2">
+            <p className="font-semibold text-[#333333] text-4xl">120</p>
+            <img src={pointimg} alt="" className="w-8 " />
+          </div>
+        </div>
+      </div>
+         <div className="bg-white  py-3 px-3 rounded-lg">
+        {/* Product Info */}
+        <div className="flex flex-col xl:flex-row items-center justify-between gap-3 lg:gap-6">
+          <img
+            src={bpimg6}
+            alt="Advanced Dissection Kit"
+            className="h-40 w-40 sm:h-48 sm:w-48 lg:h-50 lg:w-50 rounded-xl mx-auto xl:mx-0 flex-shrink-0"
+          />
+
+          <div className="w-full flex flex-col xl:flex-row items-center xl:justify-center gap-6 ">
+            <div className="flex-1 ">
+              <div className="py-3 flex flex-col lg:flex-row gap-4">
+                <p className="text-[#333333] font-medium text-lg sm:text-xl lg:text-2xl leading-tight pr-6 text-center xl:text-left">
+                  Automatic Blood Pressure
+                </p>
+              </div>
+
+              {/* Details */}
+              <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-7 gap-4 lg:gap-6 ">
+                <div>
+                  <p className="text-[#8f8f8f] text-sm">Brand:</p>
+                  <p className="font-semibold text-[#333333] text-[24px]">
+                    Rossmax
+                  </p>
+                </div>
+                <div>
+                  <p className="text-[#8f8f8f] text-sm">Country of Origin:</p>
+                  <p className="font-semibold text-[#333333] text-[24px]">
+                    America
+                  </p>
+                </div>
+                <div>
+                  <p className="text-[#8f8f8f] text-sm">Total Price:</p>
+                  <p className="font-[500] text-[24px] cursor-pointer text-nowrap">
+                    $100.00
+                  </p>
+                </div>
+               <div>
+                  <p className="text-[#8f8f8f] text-sm">Quantity:</p>
+                  <p className="font-[500] text-[24px] cursor-pointer text-nowrap">
+                    6
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="px-10 flex items-center gap-2">
+            <p className="font-semibold text-[#333333] text-4xl">120</p>
+            <img src={pointimg} alt="" className="w-8 " />
+          </div>
+        </div>
+      </div>
+         <div className="bg-white  py-3 px-3 rounded-lg">
         {/* Product Info */}
         <div className="flex flex-col xl:flex-row items-center justify-between gap-3 lg:gap-6">
           <img
@@ -66,11 +223,116 @@ function OrderDetails() {
             </div>
           </div>
           <div className="px-10 flex items-center gap-2">
-            <p className="font-semibold text-[#333333] text-2xl">120</p>
-            <img src={pointimg} alt="" className="w-10 " />
+            <p className="font-semibold text-[#333333] text-4xl">120</p>
+            <img src={pointimg} alt="" className="w-8 " />
           </div>
         </div>
       </div>
+         <div className="bg-white  py-3 px-3 rounded-lg">
+        {/* Product Info */}
+        <div className="flex flex-col xl:flex-row items-center justify-between gap-3 lg:gap-6">
+          <img
+            src={bpimg6}
+            alt="Advanced Dissection Kit"
+            className="h-40 w-40 sm:h-48 sm:w-48 lg:h-50 lg:w-50 rounded-xl mx-auto xl:mx-0 flex-shrink-0"
+          />
+
+          <div className="w-full flex flex-col xl:flex-row items-center xl:justify-center gap-6 ">
+            <div className="flex-1 ">
+              <div className="py-3 flex flex-col lg:flex-row gap-4">
+                <p className="text-[#333333] font-medium text-lg sm:text-xl lg:text-2xl leading-tight pr-6 text-center xl:text-left">
+                  Automatic Blood Pressure
+                </p>
+              </div>
+
+              {/* Details */}
+              <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-7 gap-4 lg:gap-6 ">
+                <div>
+                  <p className="text-[#8f8f8f] text-sm">Brand:</p>
+                  <p className="font-semibold text-[#333333] text-[24px]">
+                    Rossmax
+                  </p>
+                </div>
+                <div>
+                  <p className="text-[#8f8f8f] text-sm">Country of Origin:</p>
+                  <p className="font-semibold text-[#333333] text-[24px]">
+                    America
+                  </p>
+                </div>
+                <div>
+                  <p className="text-[#8f8f8f] text-sm">Total Price:</p>
+                  <p className="font-[500] text-[24px] cursor-pointer text-nowrap">
+                    $100.00
+                  </p>
+                </div>
+               <div>
+                  <p className="text-[#8f8f8f] text-sm">Quantity:</p>
+                  <p className="font-[500] text-[24px] cursor-pointer text-nowrap">
+                    6
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="px-10 flex items-center gap-2">
+            <p className="font-semibold text-[#333333] text-4xl">120</p>
+            <img src={pointimg} alt="" className="w-8 " />
+          </div>
+        </div>
+      </div>
+         <div className="bg-white  py-3 px-3 rounded-lg">
+        {/* Product Info */}
+        <div className="flex flex-col xl:flex-row items-center justify-between gap-3 lg:gap-6">
+          <img
+            src={bpimg6}
+            alt="Advanced Dissection Kit"
+            className="h-40 w-40 sm:h-48 sm:w-48 lg:h-50 lg:w-50 rounded-xl mx-auto xl:mx-0 flex-shrink-0"
+          />
+
+          <div className="w-full flex flex-col xl:flex-row items-center xl:justify-center gap-6 ">
+            <div className="flex-1 ">
+              <div className="py-3 flex flex-col lg:flex-row gap-4">
+                <p className="text-[#333333] font-medium text-lg sm:text-xl lg:text-2xl leading-tight pr-6 text-center xl:text-left">
+                  Automatic Blood Pressure
+                </p>
+              </div>
+
+              {/* Details */}
+              <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-7 gap-4 lg:gap-6 ">
+                <div>
+                  <p className="text-[#8f8f8f] text-sm">Brand:</p>
+                  <p className="font-semibold text-[#333333] text-[24px]">
+                    Rossmax
+                  </p>
+                </div>
+                <div>
+                  <p className="text-[#8f8f8f] text-sm">Country of Origin:</p>
+                  <p className="font-semibold text-[#333333] text-[24px]">
+                    America
+                  </p>
+                </div>
+                <div>
+                  <p className="text-[#8f8f8f] text-sm">Total Price:</p>
+                  <p className="font-[500] text-[24px] cursor-pointer text-nowrap">
+                    $100.00
+                  </p>
+                </div>
+               <div>
+                  <p className="text-[#8f8f8f] text-sm">Quantity:</p>
+                  <p className="font-[500] text-[24px] cursor-pointer text-nowrap">
+                    6
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="px-10 flex items-center gap-2">
+            <p className="font-semibold text-[#333333] text-4xl">120</p>
+            <img src={pointimg} alt="" className="w-8 " />
+          </div>
+        </div>
+      </div>
+     </div>
     </div>
   );
 }
