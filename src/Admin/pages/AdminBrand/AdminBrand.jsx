b6345c76d71e660852a0f1beb82dbd6f2ada4b95
Muhammad Umiar <muhammadumairjabbar89@gmail.com>
2025-08-18 13:06:27 +0500

    Brand start

--- src/Admin/pages/AdminBrand/AdminBrand.jsx
+++ src/Admin/pages/AdminBrand/AdminBrand.jsx
@@ -1,10 +1,64 @@
 import React from 'react'
+import { CiSearch } from "react-icons/ci";
+import { FiPlus } from "react-icons/fi";
+import { motion } from "framer-motion";
+import MenuImg from './Assets/MenuImg.png'
 
 const AdminBrand = () => {
   return (
-    <div>
-      AdminBrand
-    </div>
+     <div className="bg-[#fafafa] lg:ml-[295px]">
+      <style>{`::-webkit-scrollbar { display: none; }`}</style>
+ <div className="md:p-6 p-4">
+{/* Search and Add Button */}
+        <div className="flex gap-4 items-center md:flex-nowrap flex-wrap">
+          {/* Search */}
+          <motion.div
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.5, ease: "easeOut" }}
+            className="border border-[#e1e2e3] text-black bg-white px-4 py-3 w-full flex gap-3 items-center rounded-lg"
+          >
+            <CiSearch className="text-2xl" />
+            <input
+              type="text"
+              className="w-full text-[16px] outline-none"
+              placeholder="Search Brands..."
+            />
+          </motion.div>
+
+          {/* Add Button */}
+          <motion.button
+            initial={{ opacity: 0, y: -30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, ease: "easeOut", delay: 0.1 }}
+            className="bg-[#00427e] cursor-pointer  hover:bg-[#003366] transition-all text-[16px] text-white px-5 py-3 flex gap-2 md:w-auto w-full rounded-lg justify-center items-center"
+            // onClick={openModal}
+          >
+            <FiPlus className="text-lg" />
+            <span className="whitespace-nowrap">Add Brands</span>
+          </motion.button>
+        </div>
+        {/* Brands  */}
+        <div className='mt-6'>
+          <p className='font-[500] text-[24px]'>All Brands</p>
+          <div className='grid md:grid-cols-3 mt-3 grid-cols-2 gap-4'>
+            <div className=' flex justify-between items-center shadow border border-[#f0f0f1] rounded-lg p-3'>
+              <p className='font-[400] text-[25px]'>Scalpel</p>
+              <img src={MenuImg} className='w-8 '/>
+            </div>
+            <div className=' flex justify-between items-center shadow border border-[#f0f0f1] rounded-lg p-3'>
+              <p className='font-[400] text-[25px]'>Scalpel</p>
+              <img src={MenuImg} className='w-8 '/>
+            </div>
+            <div className=' flex justify-between items-center shadow border border-[#f0f0f1] rounded-lg p-3'>
+              <p className='font-[400] text-[25px]'>Scalpel</p>
+              <img src={MenuImg} className='w-8 '/>
+            </div>
+
+          </div>
+        </div>
+ </div>
+     </div>
   )
 }
 
