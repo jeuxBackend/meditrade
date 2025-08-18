d5ae79b0c0079b8eedd50b424626879158b18c6c
Noor Usman <noorusman0097@gmail.com>
2025-08-18 11:46:00 +0500

    QuoteRequests done

--- src/Website/Pages/QuoteRequests/QuoteRequests.jsx
+++ src/Website/Pages/QuoteRequests/QuoteRequests.jsx
@@ -1,24 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 import RespondRequest from "./Components/RespondRequest/RespondRequest";
 import SentOffersRequest from "./Components/SentOffersRequest/SentOffersRequest";
+import AcceptedRequest from "./Components/AcceptedRequest/AcceptedRequest";
+import RefusedRequest from "./Components/RefusedRequest/RefusedRequest";
 
 function QuoteRequests() {
+  const [activeTab, setActiveTab] = useState("respond");
+
   return (
     <div className="px-4 md:px-7 min-h-screen py-4 mt-4">
       <div className="grid sm:grid-cols-2 md:grid-cols-5 lg:grid-cols-8 gap-y-3 gap-x-6">
-        <p className="bg-[#00427e] py-3 text-center rounded-lg text-white cursor-pointer">
+        <p
+          onClick={() => setActiveTab("respond")}
+          className={`py-3 text-center rounded-lg cursor-pointer ${
+            activeTab === "respond" ? "bg-[#00427e] text-white" : "bg-white"
+          }`}
+        >
           Respond
         </p>
-        <p className="bg-white py-3 text-center rounded-lg cursor-pointer text-nowrap">
+
+        <p
+          onClick={() => setActiveTab("offers")}
+          className={`py-3 text-center rounded-lg cursor-pointer ${
+            activeTab === "offers" ? "bg-[#00427e] text-white" : "bg-white"
+          }`}
+        >
           Offers Sent
         </p>
-        <p className="bg-white py-3 text-center rounded-lg cursor-pointer">
+
+        <p
+          onClick={() => setActiveTab("accepted")}
+          className={`py-3 text-center rounded-lg cursor-pointer ${
+            activeTab === "accepted" ? "bg-[#00427e] text-white" : "bg-white"
+          }`}
+        >
           Accepted
         </p>
-        <p className="bg-white py-3 text-center rounded-lg cursor-pointer">Refused</p>
+
+        <p
+          onClick={() => setActiveTab("refused")}
+          className={`py-3 text-center rounded-lg cursor-pointer ${
+            activeTab === "refused" ? "bg-[#00427e] text-white" : "bg-white"
+          }`}
+        >
+          Refused
+        </p>
       </div>
-        <RespondRequest />
-        <SentOffersRequest />
+      <div className="mt-6">
+        {activeTab === "respond" && <RespondRequest />}
+        {activeTab === "offers" && <SentOffersRequest />}
+        {activeTab === "accepted" && <AcceptedRequest />}
+        {activeTab === "refused" && <RefusedRequest />}
+      </div>
     </div>
   );
 }
